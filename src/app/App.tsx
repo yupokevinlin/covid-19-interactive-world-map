cf4bf487d5b32055909d9bebe899083f3dc1347d
Kevin <yupolin921021@gmail.com>
2020-03-08 16:14:38 -0700

    Added redux store provider.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { configureStore } from "../state/store";
 
 export interface AppProps {}
 
 const App: React.FC<AppProps> = props => {
-  return <div className={"app"} />;
+  return (
+    <Provider store={configureStore()}>
+      <div className={"app"} />
+    </Provider>
+  );
 };
 
 export default App;
