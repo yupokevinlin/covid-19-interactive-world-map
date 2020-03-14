95aab66e0fc0d04d490ea5412cca6d9db9baf98f
Kevin <yupolin921021@gmail.com>
2020-03-14 11:46:31 -0700

    Added MapPageContainer.

---
+++ src/display/containers/MapPageContainer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface MapPageContainerProps {}
+
+const MapPageContainer: React.FC<MapPageContainerProps> = props => {
+  return <div />;
+};
+
+export default MapPageContainer;
