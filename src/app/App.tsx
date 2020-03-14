c192d64a7830e1958fadcdf447ec0e6dd498b4c9
Kevin <yupolin921021@gmail.com>
2020-03-14 11:49:18 -0700

    App container will now display the entire page.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { configureStore } from "../state/store";
+import styled from "styled-components";
 
 export interface AppProps {}
+
+const StyledApp = styled.div`
+  height: 100%;
+  width: 100%;
+`;
 
 const App: React.FC<AppProps> = props => {
   return (
     <Provider store={configureStore()}>
-      <div className={"app"} />
+      <StyledApp className={"app"} />
     </Provider>
   );
 };
