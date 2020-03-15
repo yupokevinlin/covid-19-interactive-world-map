1fd5b0b4585b240155f02126afb51c42aae1361a
Kevin <yupolin921021@gmail.com>
2020-03-15 01:00:47 -0700

    Added navigation components to App.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,18 +2,27 @@ import React from "react";
 import { Provider } from "react-redux";
 import { configureStore } from "../state/store";
 import styled from "styled-components";
+import MapPageContainer from "../display/containers/MapPageContainer";
+import PageWrapperContainer from "../display/containers/PageWrapperContainer";
 
 export interface AppProps {}
 
 const StyledApp = styled.div`
   height: 100%;
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
 `;
 
 const App: React.FC<AppProps> = props => {
   return (
     <Provider store={configureStore()}>
-      <StyledApp className={"app"} />
+      <StyledApp className={"app"}>
+        <PageWrapperContainer>
+          <MapPageContainer />
+        </PageWrapperContainer>
+      </StyledApp>
     </Provider>
   );
 };
