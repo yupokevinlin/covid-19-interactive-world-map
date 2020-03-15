f2756f2654bd20bc2d61398fa4622fc3fdd78b70
Kevin <yupolin921021@gmail.com>
2020-03-15 00:59:42 -0700

    MapPageContainer will now display MapPage.

--- src/display/containers/MapPageContainer.tsx
+++ src/display/containers/MapPageContainer.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import MapPage from "../pages/MapPage";
 
 export interface MapPageContainerProps {}
 
 const MapPageContainer: React.FC<MapPageContainerProps> = props => {
-  return <div />;
+  return <MapPage />;
 };
 
 export default MapPageContainer;
