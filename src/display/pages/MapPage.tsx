6aac7ba867f91770be105e0c4fc990b139f515cb
Kevin <yupolin921021@gmail.com>
2020-03-15 19:11:32 -0700

    Added testing code for MapPage.

--- src/display/pages/MapPage.tsx
+++ src/display/pages/MapPage.tsx
@@ -1,9 +1,53 @@
 import React from "react";
+import ESRIMap, { MapPolygon } from "../components/ESRIMap/ESRIMap";
+import { CountryOutline, LatLon } from "../../api/MapApi/types";
+import { MapApi } from "../../api/MapApi/MapApi";
 
 export interface MapPageProps {}
 
 const MapPage: React.FC<MapPageProps> = props => {
-  return <div />;
+  const countryOutlines: Array<CountryOutline> = MapApi.getCountryOutlines();
+  const testNumbers: Array<number> = [
+    0,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    10,
+    10,
+    10,
+    10,
+    10,
+    10,
+    100,
+    100,
+    100,
+    1000,
+    1000,
+    100000,
+    1000000,
+  ];
+  const mapPolygons: Array<MapPolygon> = countryOutlines.map((countryOutline, index) => {
+    const cases: number = testNumbers[Math.floor(Math.random() * testNumbers.length)];
+    return {
+      internalId: index,
+      name: countryOutline.name.en,
+      confirmedCasesCount: cases,
+      geometry: countryOutline.geometry,
+    };
+  });
+  return <ESRIMap mapPolygons={mapPolygons} />;
 };
 
 export default MapPage;
