f6b35e8b404c94acc27a9d954f1e143289577404
Kevin <yupolin921021@gmail.com>
2020-11-12 18:36:32 -0800

    Added helper method to get name array from hierarchical name.

--- shared/helpers/General.ts
+++ shared/helpers/General.ts
@@ -1,3 +1,7 @@
 export const getHierarchicalName = (name: Array<string>): string => {
   return name.join(".");
 };
+
+export const getNameArray = (hierarchicalName: string): Array<string> => {
+  return hierarchicalName.split(".");
+};
