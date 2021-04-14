689e94cc1e29127c6c52a81a0b1b20030af45d4a
Kevin <yupolin921021@gmail.com>
2021-04-13 23:42:49 -0700

    Added MapPageState to root reducer and root saga.

--- client/src/state/rootReducer.ts
+++ client/src/state/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as appReducer } from "./global/App/reducer";
+import { reducer as mapPageReducer } from "./containers/MapPageContainer/reducer";
 
 export const rootReducer = combineReducers({
   app: appReducer,
+  mapPage: mapPageReducer,
 });
 
 export default rootReducer;
--- client/src/state/rootSaga.ts
+++ client/src/state/rootSaga.ts
@@ -1,9 +1,15 @@
 import { all } from "redux-saga/effects";
 import AppSagas from "./global/App/sagas";
+import MapPageSagas from "./containers/MapPageContainer/sagas";
 
 function* rootSaga(): any {
   yield all({
-    ...AppSagas,
+    app: all({
+      ...AppSagas,
+    }),
+    mapPage: all({
+      ...MapPageSagas,
+    }),
   });
 }
 
