30f8c6246ded6c19015e1e9121ecd5a7d1640729
Kevin <yupolin921021@gmail.com>
2021-04-16 01:30:47 -0700

    Added tree related utility functions.

--- shared/helpers/General.ts
+++ shared/helpers/General.ts
@@ -1,7 +1,38 @@
+import {TreeItem} from "../types/data/Tree/TreeTypes";
+
 export const getHierarchicalName = (name: Array<string>): string => {
   return name.join("_");
 };
 
 export const getNameArray = (hierarchicalName: string): Array<string> => {
   return hierarchicalName.split("_");
+};
+
+export const getSequentialHierarchicalNames = (hierarchicalName: string): Array<string> => {
+  const name: Array<string> = getNameArray(hierarchicalName);
+  const hierarchicalNames: Array<string> = [];
+  name.forEach((s, index) => {
+    const currentName: Array<string> = [];
+    for (let i = 0; i < index + 1; i++) {
+      currentName.push(name[i]);
+    }
+    hierarchicalNames.push(getHierarchicalName(currentName));
+  });
+  return hierarchicalNames;
+};
+
+export const getTreeItem = (dataTree: TreeItem, hierarchicalName: string): TreeItem | undefined => {
+  const name: Array<string> = getNameArray(hierarchicalName);
+  let matchingItem: TreeItem | undefined = dataTree;
+  let currentName: Array<string> = [];
+  name.forEach((s, index) => {
+    currentName.push(s);
+    const currentHierarchicalName: string = getHierarchicalName(currentName);
+    if (matchingItem) {
+      if (matchingItem.hierarchicalName !== currentHierarchicalName) {
+        matchingItem = matchingItem.children.find((child) => child.hierarchicalName === currentHierarchicalName);
+      }
+    }
+  });
+  return matchingItem;
 };
