3c764c4e22a24ab2c97c9f3d87f181011eac9de1
Kevin <yupolin921021@gmail.com>
2021-04-17 19:23:59 -0700

    Added region change handling logic.

--- client/src/state/containers/MapPageContainer/sagas.ts
+++ client/src/state/containers/MapPageContainer/sagas.ts
@@ -3,11 +3,13 @@ import {MapPageActionTypes} from "./types";
 import {MapPageHandleRegionChangeAction, MapPageInitAction} from "./actions";
 import {MapPolygon} from "../../../../../shared/types/data/Map/MapTypes";
 import {MapApi} from "../../../api/MapApi/MapApi";
-import {CasesData} from "../../../../../shared/types/data/Cases/CasesTypes";
+import {CasesData, CasesDataObject} from "../../../../../shared/types/data/Cases/CasesTypes";
 import {CasesApi} from "../../../api/CasesApi/CasesApi";
 import {DateUtils} from "../../../helper/DateUtils";
 import {ESRIMapPolygon} from "../../../display/components/ESRIMap/types";
 import {getSequentialHierarchicalNames} from "../../../../../shared/helpers/General";
+import {Store} from "../../store";
+import {AppState} from "../../global/App/types";
 
 export const mapPageSagas = {
   initSaga: takeEvery(MapPageActionTypes.INIT, initSaga),
@@ -19,10 +21,12 @@ function * initSaga(action: MapPageInitAction): any {
     type: MapPageActionTypes.SET_MAP_POLYGONS,
     mapPolygons: [],
   });
-  const esriMapPolygons: Array<ESRIMapPolygon> = yield call(getEsriMapPolygonsByHierarchicalName, "World");
+  const layer0MapPolygons: Array<MapPolygon> = yield call(MapApi.getMapLayer0Data);
+  const hierarchicalNames: Array<string> = layer0MapPolygons.map((mapPolygon) => mapPolygon.hierarchicalName);
+  const casesData: Array<CasesData> = yield call(CasesApi.getMultipleCasesData, hierarchicalNames);
   yield put({
     type: MapPageActionTypes.SET_MAP_POLYGONS,
-    mapPolygons: esriMapPolygons,
+    mapPolygons: getESRIMapPolygons(layer0MapPolygons, casesData),
   });
 
   const dateValues: Array<string> = DateUtils.getDateStringArray("1/1/20", DateUtils.getCurrentDate());
@@ -33,63 +37,49 @@ function * initSaga(action: MapPageInitAction): any {
 }
 
 function * handleRegionChange(action: MapPageHandleRegionChangeAction): any {
-  const esriMapPolygons: Array<ESRIMapPolygon> = yield call(getEsriMapPolygonsByHierarchicalName, action.hierarchicalName);
-  yield put({
-    type: MapPageActionTypes.SET_MAP_POLYGONS,
-    mapPolygons: esriMapPolygons,
-  });
-}
-
-const getEsriMapPolygonsByHierarchicalName = (hierarchicalName: string): Promise<Array<ESRIMapPolygon>> => {
-  return new Promise<Array<ESRIMapPolygon>>((resolve, reject) => {
-    MapApi.getMapLayer0Data().then((mapPolygons) => {
-      getMapPolygonsByHierarchicalName(mapPolygons, hierarchicalName).then((filteredMapPolygons) => {
-        const hierarchicalNames: Array<string> = filteredMapPolygons.map((mapPolygon) => mapPolygon.hierarchicalName);
-        getCasesData(hierarchicalNames).then((casesData) => {
-          const esriMapPolygons: Array<ESRIMapPolygon> = getESRIMapPolygons(filteredMapPolygons, casesData);
-          resolve(esriMapPolygons);
-        });
+  const appState: AppState = yield select(getAppStateSelector);
+  const casesDataObject: CasesDataObject = appState.casesDataObject;
+  const layer0MapPolygons: Array<MapPolygon> = yield call(MapApi.getMapLayer0Data);
+  if (action.hierarchicalName === "World") {
+    const hierarchicalNames: Array<string> = layer0MapPolygons.map((mapPolygon) => mapPolygon.hierarchicalName);
+    const casesData: Array<CasesData> = getCasesDataByHierarchicalNames(casesDataObject, hierarchicalNames);
+    yield put({
+      type: MapPageActionTypes.SET_MAP_POLYGONS,
+      mapPolygons: getESRIMapPolygons(layer0MapPolygons, casesData),
+    });
+  } else {
+    let filteredMapPolygons: Array<MapPolygon> = layer0MapPolygons;
+    const sequentialHierarchicalNames: Array<string> = getSequentialHierarchicalNames(action.hierarchicalName);
+    if (sequentialHierarchicalNames.length === 2) {
+      const layer1HierarchicalName: string = sequentialHierarchicalNames[1];
+      const layer1MapPolygons: Array<MapPolygon> = yield call(MapApi.getMapLayer1Data, layer1HierarchicalName);
+      filteredMapPolygons.push(...layer1MapPolygons);
+      filteredMapPolygons = filteredMapPolygons.filter((mapPolygon) => mapPolygon.hierarchicalName !== layer1HierarchicalName);
+      const hierarchicalNames: Array<string> = filteredMapPolygons.map((mapPolygon) => mapPolygon.hierarchicalName);
+      const casesData: Array<CasesData> = getCasesDataByHierarchicalNames(casesDataObject, hierarchicalNames);
+      yield put({
+        type: MapPageActionTypes.SET_MAP_POLYGONS,
+        mapPolygons: getESRIMapPolygons(filteredMapPolygons, casesData),
       });
-    });
-  });
-};
-
-const getMapPolygonsByHierarchicalName = (mapPolygons: Array<MapPolygon>, hierarchicalName: string): Promise<Array<MapPolygon>> => {
-  return new Promise<Array<MapPolygon>>((resolve, reject) => {
-    if (hierarchicalName === "World") {
-      resolve(mapPolygons);
-    } else {
-      let filteredMapPolygons: Array<MapPolygon> = mapPolygons;
-      const sequentialHierarchicalNames: Array<string> = getSequentialHierarchicalNames(hierarchicalName);
-      if (sequentialHierarchicalNames.length === 2) {
-        filteredMapPolygons = filteredMapPolygons.filter((mapPolygon) => mapPolygon.hierarchicalName !== sequentialHierarchicalNames[1]);
-        MapApi.getMapLayer1Data(sequentialHierarchicalNames[1]).then((layer1MapPolygons) => {
-          filteredMapPolygons.push(...layer1MapPolygons);
-          resolve(filteredMapPolygons);
-        });
-      } else if (sequentialHierarchicalNames.length === 3) {
-        filteredMapPolygons = filteredMapPolygons.filter((mapPolygon) => mapPolygon.hierarchicalName !== sequentialHierarchicalNames[1]);
-        MapApi.getMapLayer1Data(sequentialHierarchicalNames[1]).then((layer1MapPolygons) => {
-          filteredMapPolygons.push(...layer1MapPolygons);
-          filteredMapPolygons = filteredMapPolygons.filter((mapPolygon) => mapPolygon.hierarchicalName !== sequentialHierarchicalNames[2]);
-          MapApi.getMapLayer2Data(sequentialHierarchicalNames[2]).then((layer2MapPolygons) => {
-            filteredMapPolygons.push(...layer2MapPolygons);
-            resolve(filteredMapPolygons);
-          });
-        });
-      }
+    } else if (sequentialHierarchicalNames.length === 3) {
+      const layer1HierarchicalName: string = sequentialHierarchicalNames[1];
+      const layer2HierarchicalName: string = sequentialHierarchicalNames[2];
+      const layersPromises: Array<Promise<Array<MapPolygon>>> = [
+        MapApi.getMapLayer1Data(layer1HierarchicalName),
+        MapApi.getMapLayer2Data(layer2HierarchicalName),
+      ];
+      const layersMapPolygonsData: Array<Array<MapPolygon>> = yield all(layersPromises);
+      filteredMapPolygons.push(...layersMapPolygonsData[0], ...layersMapPolygonsData[1]);
+      filteredMapPolygons = filteredMapPolygons.filter((mapPolygon) => mapPolygon.hierarchicalName !== layer1HierarchicalName && mapPolygon.hierarchicalName !== layer2HierarchicalName);
+      const hierarchicalNames: Array<string> = filteredMapPolygons.map((mapPolygon) => mapPolygon.hierarchicalName);
+      const casesData: Array<CasesData> = getCasesDataByHierarchicalNames(casesDataObject, hierarchicalNames);
+      yield put({
+        type: MapPageActionTypes.SET_MAP_POLYGONS,
+        mapPolygons: getESRIMapPolygons(filteredMapPolygons, casesData),
+      });
     }
-  });
-};
-
-const getCasesData = (hierarchicalNames: Array<string>): Promise<Array<CasesData>> => {
-  return new Promise<Array<CasesData>>(((resolve, reject) => {
-    const getCasesDataArrayPromises: Array<Promise<CasesData>> = hierarchicalNames.map((hierarchicalName) => CasesApi.getCasesData(hierarchicalName));
-    Promise.all(getCasesDataArrayPromises).then((values) => {
-      resolve(values);
-    });
-  }));
-};
+  }
+}
 
 const getESRIMapPolygons = (mapPolygons: Array<MapPolygon>, casesData: Array<CasesData>): Array<ESRIMapPolygon> => {
   return mapPolygons.map((mapPolygon, index) => {
@@ -100,5 +90,13 @@ const getESRIMapPolygons = (mapPolygons: Array<MapPolygon>, casesData: Array<Cas
     }
   });
 };
+
+export const getCasesDataByHierarchicalNames = (casesDataObject: CasesDataObject, hierarchicalNames: Array<string>): Array<CasesData | undefined> => {
+  return hierarchicalNames.map((hierarchicalName) => casesDataObject[hierarchicalName]);
+};
+
+function getAppStateSelector(store: Store): AppState {
+  return store.app;
+}
 
 export default mapPageSagas;
