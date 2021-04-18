812cd5179468feef59ff5c4356b08b0f744dd310
Kevin <yupolin921021@gmail.com>
2021-04-18 02:39:10 -0700

    Fixed map polygon logic to handle all cases.

--- client/src/state/containers/MapPageContainer/sagas.ts
+++ client/src/state/containers/MapPageContainer/sagas.ts
@@ -1,4 +1,4 @@
-import {call, put, select, takeEvery, all} from "redux-saga/effects";
+import {call, put, select, takeEvery, all, delay} from "redux-saga/effects";
 import {MapPageActionTypes} from "./types";
 import {MapPageHandleRegionChangeAction, MapPageInitAction} from "./actions";
 import {MapPolygon} from "../../../../../shared/types/data/Map/MapTypes";
@@ -58,45 +58,121 @@ function * handleRegionChange(action: MapPageHandleRegionChangeAction): any {
     let filteredMapPolygons: Array<MapPolygon> = layer0MapPolygons;
     const sequentialHierarchicalNames: Array<string> = getSequentialHierarchicalNames(action.hierarchicalName);
     if (sequentialHierarchicalNames.length === 2) {
-      const layer1HierarchicalName: string = sequentialHierarchicalNames[1];
-      const matchingTreeItem: TreeItem = getTreeItem(dataTree, layer1HierarchicalName);
+      const layer0HierarchicalName: string = sequentialHierarchicalNames[1];
+      const matchingTreeItem: TreeItem = getTreeItem(dataTree, layer0HierarchicalName);
       if (matchingTreeItem.children.length > 0) {
-        const layer1MapPolygons: Array<MapPolygon> = yield call(MapApi.getMapLayer1Data, layer1HierarchicalName);
+        const layer1MapPolygons: Array<MapPolygon> = yield call(MapApi.getMapLayer1Data, layer0HierarchicalName);
         filteredMapPolygons.push(...layer1MapPolygons);
-        filteredMapPolygons = filteredMapPolygons.filter((mapPolygon) => mapPolygon.hierarchicalName !== layer1HierarchicalName);
+        const layer0MatchingPolygon: MapPolygon = filteredMapPolygons.find((mapPolygon) => mapPolygon.hierarchicalName === layer0HierarchicalName);
+        filteredMapPolygons = filteredMapPolygons.filter((mapPolygon) => mapPolygon.hierarchicalName !== layer0HierarchicalName);
         const hierarchicalNames: Array<string> = filteredMapPolygons.map((mapPolygon) => mapPolygon.hierarchicalName);
         const casesData: Array<CasesData> = getCasesDataByHierarchicalNames(casesDataObject, hierarchicalNames);
         yield put({
           type: MapPageActionTypes.SET_MAP_POLYGONS,
           mapPolygons: getESRIMapPolygons(filteredMapPolygons, casesData),
         });
-      } else {
-        const matchingPolygon: MapPolygon = filteredMapPolygons.find((mapPolygon) => mapPolygon.hierarchicalName === layer1HierarchicalName);
-        if (!!matchingPolygon) {
+        if (!!layer0MatchingPolygon) {
           yield put({
             type: MapPageActionTypes.SET_FOCUS_MAP_GEOMETRY,
-            focusMapGeometry: matchingPolygon.geometry,
+            focusMapGeometry: layer0MatchingPolygon.geometry,
+          });
+        }
+      } else {
+        const hierarchicalNames: Array<string> = layer0MapPolygons.map((mapPolygon) => mapPolygon.hierarchicalName);
+        const casesData: Array<CasesData> = getCasesDataByHierarchicalNames(casesDataObject, hierarchicalNames);
+        yield put({
+          type: MapPageActionTypes.SET_MAP_POLYGONS,
+          mapPolygons: getESRIMapPolygons(layer0MapPolygons, casesData),
+        });
+        const layer0MatchingPolygon: MapPolygon = filteredMapPolygons.find((mapPolygon) => mapPolygon.hierarchicalName === layer0HierarchicalName);
+        if (!!layer0MatchingPolygon) {
+          yield put({
+            type: MapPageActionTypes.SET_FOCUS_MAP_GEOMETRY,
+            focusMapGeometry: layer0MatchingPolygon.geometry,
           });
         }
       }
     } else if (sequentialHierarchicalNames.length === 3) {
-      const layer1HierarchicalName: string = sequentialHierarchicalNames[1];
-      const layer2HierarchicalName: string = sequentialHierarchicalNames[2];
+      const layer0HierarchicalName: string = sequentialHierarchicalNames[1];
+      const layer1HierarchicalName: string = sequentialHierarchicalNames[2];
+      const matchingTreeItem: TreeItem = getTreeItem(dataTree, layer1HierarchicalName);
+      if (matchingTreeItem.children.length > 0) {
+        const layersPromises: Array<Promise<Array<MapPolygon>>> = [
+          MapApi.getMapLayer1Data(layer0HierarchicalName),
+          MapApi.getMapLayer2Data(layer1HierarchicalName),
+        ];
+        const layersMapPolygonsData: Array<Array<MapPolygon>> = yield all(layersPromises);
+        filteredMapPolygons.push(...layersMapPolygonsData[0], ...layersMapPolygonsData[1]);
+        const layer1MatchingPolygon: MapPolygon = filteredMapPolygons.find((mapPolygon) => mapPolygon.hierarchicalName === layer1HierarchicalName);
+        filteredMapPolygons = filteredMapPolygons.filter((mapPolygon) => mapPolygon.hierarchicalName !== layer0HierarchicalName && mapPolygon.hierarchicalName !== layer1HierarchicalName);
+        const hierarchicalNames: Array<string> = filteredMapPolygons.map((mapPolygon) => mapPolygon.hierarchicalName);
+        const casesData: Array<CasesData> = getCasesDataByHierarchicalNames(casesDataObject, hierarchicalNames);
+        yield put({
+          type: MapPageActionTypes.SET_MAP_POLYGONS,
+          mapPolygons: getESRIMapPolygons(filteredMapPolygons, casesData),
+        });
+        if (!!layer1MatchingPolygon) {
+          yield put({
+            type: MapPageActionTypes.SET_FOCUS_MAP_GEOMETRY,
+            focusMapGeometry: layer1MatchingPolygon.geometry,
+          });
+        }
+      } else {
+        const layer1MapPolygons: Array<MapPolygon> = yield call(MapApi.getMapLayer1Data, layer0HierarchicalName);
+        filteredMapPolygons.push(...layer1MapPolygons);
+        const layer0MatchingPolygon: MapPolygon = filteredMapPolygons.find((mapPolygon) => mapPolygon.hierarchicalName === layer0HierarchicalName);
+        filteredMapPolygons = filteredMapPolygons.filter((mapPolygon) => mapPolygon.hierarchicalName !== layer0HierarchicalName);
+        const hierarchicalNames: Array<string> = filteredMapPolygons.map((mapPolygon) => mapPolygon.hierarchicalName);
+        const casesData: Array<CasesData> = getCasesDataByHierarchicalNames(casesDataObject, hierarchicalNames);
+        yield put({
+          type: MapPageActionTypes.SET_MAP_POLYGONS,
+          mapPolygons: getESRIMapPolygons(filteredMapPolygons, casesData),
+        });
+        if (!!layer0MatchingPolygon) {
+          yield put({
+            type: MapPageActionTypes.SET_FOCUS_MAP_GEOMETRY,
+            focusMapGeometry: layer0MatchingPolygon.geometry,
+          });
+        }
+        const layer1MatchingPolygon: MapPolygon = filteredMapPolygons.find((mapPolygon) => mapPolygon.hierarchicalName === layer1HierarchicalName);
+        if (!!layer1MatchingPolygon) {
+          yield put({
+            type: MapPageActionTypes.SET_FOCUS_MAP_GEOMETRY,
+            focusMapGeometry: layer1MatchingPolygon.geometry,
+          });
+        }
+      }
+    } else if (sequentialHierarchicalNames.length === 4) {
+      const layer0HierarchicalName: string = sequentialHierarchicalNames[1];
+      const layer1HierarchicalName: string = sequentialHierarchicalNames[2];
+      const layer2HierarchicalName: string = sequentialHierarchicalNames[3];
       const layersPromises: Array<Promise<Array<MapPolygon>>> = [
-        MapApi.getMapLayer1Data(layer1HierarchicalName),
-        MapApi.getMapLayer2Data(layer2HierarchicalName),
+        MapApi.getMapLayer1Data(layer0HierarchicalName),
+        MapApi.getMapLayer2Data(layer1HierarchicalName),
       ];
       const layersMapPolygonsData: Array<Array<MapPolygon>> = yield all(layersPromises);
       filteredMapPolygons.push(...layersMapPolygonsData[0], ...layersMapPolygonsData[1]);
-      filteredMapPolygons = filteredMapPolygons.filter((mapPolygon) => mapPolygon.hierarchicalName !== layer1HierarchicalName && mapPolygon.hierarchicalName !== layer2HierarchicalName);
+      const layer1MatchingPolygon: MapPolygon = filteredMapPolygons.find((mapPolygon) => mapPolygon.hierarchicalName === layer1HierarchicalName);
+      filteredMapPolygons = filteredMapPolygons.filter((mapPolygon) => mapPolygon.hierarchicalName !== layer0HierarchicalName && mapPolygon.hierarchicalName !== layer1HierarchicalName);
       const hierarchicalNames: Array<string> = filteredMapPolygons.map((mapPolygon) => mapPolygon.hierarchicalName);
       const casesData: Array<CasesData> = getCasesDataByHierarchicalNames(casesDataObject, hierarchicalNames);
       yield put({
         type: MapPageActionTypes.SET_MAP_POLYGONS,
         mapPolygons: getESRIMapPolygons(filteredMapPolygons, casesData),
       });
-    } else if (sequentialHierarchicalNames.length === 4) {
-
+      if (!!layer1MatchingPolygon) {
+        yield put({
+          type: MapPageActionTypes.SET_FOCUS_MAP_GEOMETRY,
+          focusMapGeometry: layer1MatchingPolygon.geometry,
+        });
+      }
+      const layer2MatchingPolygon: MapPolygon = filteredMapPolygons.find((mapPolygon) => mapPolygon.hierarchicalName === layer2HierarchicalName);
+      if (!!layer2MatchingPolygon) {
+        yield put({
+          type: MapPageActionTypes.SET_FOCUS_MAP_GEOMETRY,
+          focusMapGeometry: layer2MatchingPolygon.geometry,
+        });
+      }
     }
   }
 }
