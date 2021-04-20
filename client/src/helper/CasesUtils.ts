178f328ee603fffec39d66c3259a950e3118920e
Kevin <yupolin921021@gmail.com>
2021-04-20 11:33:52 -0700

    Added additional helper functions.

--- client/src/helper/CasesUtils.ts
+++ client/src/helper/CasesUtils.ts
@@ -1,6 +1,13 @@
-import {DailyCasesData, DailyCasesDataNull, DailyCasesDataObject} from "../../../shared/types/data/Cases/CasesTypes";
+import {
+  CasesInformationDataObject,
+  DailyCasesData,
+  DailyCasesDataNull,
+  DailyCasesDataObject, DailyCasesInformationData, DailyCasesInformationDataNull,
+  DailyCasesInformationDataObject
+} from "../../../shared/types/data/Cases/CasesTypes";
 import {Moment} from "moment";
 import {DateUtils} from "./DateUtils";
+import {CasesDataTypes} from "../state/global/App/types";
 
 export namespace CasesUtils {
   import getDateStringFromMomentDate = DateUtils.getDateStringFromMomentDate;
@@ -28,6 +35,59 @@ export namespace CasesUtils {
             }
           }
         }
+      }
+    }
+  };
+  export const getDailyCasesInformationData = (dailyCasesInformationDataObject: DailyCasesInformationDataObject, dateString: string): DailyCasesInformationData | DailyCasesInformationDataNull => {
+    if (!dailyCasesInformationDataObject) {
+      return {
+        cases: null,
+        recoveries: null,
+        deaths: null,
+      }
+    }
+    const data: DailyCasesInformationData | undefined = dailyCasesInformationDataObject[dateString];
+    if (!!data) {
+      return data;
+    } else {
+      const date: Moment = getMomentDateFromDateString(dateString);
+      for (let i = 0; i < 10; i++) {
+        date.subtract(1, "days");
+        const previousDayData: DailyCasesInformationData | undefined = dailyCasesInformationDataObject[getDateStringFromMomentDate(date)];
+        if (!!previousDayData) {
+          return previousDayData;
+        } else {
+          if (i === 9) {
+            console.error(`Unable to get cases data on day: ${dateString}.`);
+            console.error("Daily Cases Object");
+            console.log(dailyCasesInformationDataObject);
+            return {
+              cases: null,
+              recoveries: null,
+              deaths: null,
+            }
+          }
+        }
+      }
+    }
+  };
+
+  export const getCasesInformationDataObject = (casesDataType: CasesDataTypes, dailyCasesInformationDataObject: CasesInformationDataObject, weeklyCasesInformationDataObject: CasesInformationDataObject, monthlyCasesInformationDataObject: CasesInformationDataObject, yearlyCasesInformationDataObject: CasesInformationDataObject): CasesInformationDataObject => {
+    switch (casesDataType) {
+      case CasesDataTypes.Daily: {
+        return dailyCasesInformationDataObject;
+      }
+      case CasesDataTypes.Weekly: {
+        return weeklyCasesInformationDataObject;
+      }
+      case CasesDataTypes.Monthly: {
+        return monthlyCasesInformationDataObject;
+      }
+      case CasesDataTypes.Yearly: {
+        return yearlyCasesInformationDataObject;
+      }
+      default: {
+        return null;
       }
     }
   };
