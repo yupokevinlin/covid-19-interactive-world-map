023ab15c000283ca666741b0ef8e5e20483e87e3
Kevin <yupolin921021@gmail.com>
2021-04-21 01:31:29 -0700

    Fixed a bug where the chart page will not show even when it has finished loading.

--- client/src/state/containers/ChartPageContainer/sagas.ts
+++ client/src/state/containers/ChartPageContainer/sagas.ts
@@ -1,6 +1,7 @@
 import {call, put, select, takeEvery, all, delay} from "redux-saga/effects";
 import {ChartPageActionTypes} from "./types";
 import {ChartPageInitAction} from "./actions";
+import {AppActionTypes} from "../../global/App/types";
 
 export const chartPageSagas = {
   initSaga: takeEvery(ChartPageActionTypes.INIT, initSaga),
@@ -10,6 +11,11 @@ function * initSaga(action: ChartPageInitAction): any {
   yield put({
     type: ChartPageActionTypes.SET_INIT_COMPLETE,
     initComplete: true,
+  });
+  yield put({
+    type: AppActionTypes.SET_IS_LOADING,
+    displayLoadingBar: false,
+    displayLoadingPage: false,
   });
 }
 
