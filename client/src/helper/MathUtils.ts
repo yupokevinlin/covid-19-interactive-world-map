1f533b1f08313b7fe04ffd734baf6ab2b50eb2ce
Kevin <yupolin921021@gmail.com>
2021-04-20 23:08:29 -0700

    Added optional digits to abbreviate number function.

--- client/src/helper/MathUtils.ts
+++ client/src/helper/MathUtils.ts
@@ -3,8 +3,8 @@ export namespace MathUtils {
     return Math.log(value) / Math.log(base);
   };
 
-  export const abbreviateNumber = (number: number): string => {
-    const SI_SYMBOL: Array<string> = ["", "k", "M", "G", "T", "P", "E"];
+  export const abbreviateNumber = (number: number, digits?: number): string => {
+    const SI_SYMBOL: Array<string> = ["", "k", "M", "B", "T", "P", "E"];
 
     const tier: number = (Math.log10(number) / 3) | 0;
 
@@ -15,6 +15,6 @@ export namespace MathUtils {
     const suffix: string = SI_SYMBOL[tier];
     const scale: number = Math.pow(10, tier * 3);
     const scaled: number = number / scale;
-    return scaled.toFixed(0) + suffix;
+    return scaled.toFixed(digits || 0) + suffix;
   };
 }
