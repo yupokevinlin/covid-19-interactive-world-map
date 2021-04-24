125238b101bc2c1904eea84b1e912d31cda8eedc
Kevin <yupolin921021@gmail.com>
2021-04-24 15:40:01 -0700

    Added init for home page.

--- client/src/display/containers/HomePageContainer/HomePageContainer.tsx
+++ client/src/display/containers/HomePageContainer/HomePageContainer.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, {useEffect} from "react";
 import HomePage from "../../pages/HomePage";
 import {AppActionTypes, AppState} from "../../../state/global/App/types";
 import {Store} from "../../../state/store";
 import {Dispatch} from "redux";
 import {AppAction} from "../../../state/global/App/actions";
 import {shallowEqual, useDispatch, useSelector} from "react-redux";
+import {MapPageActionTypes} from "../../../state/containers/MapPageContainer/types";
+import {HomePageActionTypes, HomePageState} from "../../../state/containers/HomePageContainer/types";
+import {HomePageAction} from "../../../state/containers/HomePageContainer/actions";
 
 export type HomePageContainerProps = HomePageContainerDataProps & HomePageContainerStyleProps & HomePageContainerEventProps;
 
@@ -23,17 +26,17 @@ export interface HomePageContainerEventProps {
 const HomePageContainer: React.FC<HomePageContainerProps> = (props) => {
   const appState: AppState = useSelector<Store, AppState>(store => store.app, shallowEqual);
   const appDispatch: Dispatch<AppAction> = useDispatch<Dispatch<AppAction>>();
+  const homePageState: HomePageState = useSelector<Store, HomePageState>(store => store.homePage, shallowEqual);
+  const homePageDispatch: Dispatch<HomePageAction> = useDispatch<Dispatch<HomePageAction>>();
 
-  const handleLoaded = (): void => {
-    appDispatch({
-      type: AppActionTypes.SET_IS_LOADING,
-      displayLoadingBar: false,
-      displayLoadingPage: false,
+  useEffect(() => {
+    homePageDispatch({
+      type: HomePageActionTypes.INIT,
     });
-  };
+  }, []);
 
   return (
-    <HomePage handleLoaded={handleLoaded}/>
+    <HomePage/>
   )
 };
 
