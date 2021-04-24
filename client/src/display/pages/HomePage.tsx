333f2d3ce03346e6b157c83118f7536a8c2053a6
Kevin <yupolin921021@gmail.com>
2021-04-24 15:38:23 -0700

    Removed handleLoaded function.

--- client/src/display/pages/HomePage.tsx
+++ client/src/display/pages/HomePage.tsx
@@ -1,4 +1,6 @@
-import React, {useEffect} from "react";
+import React from "react";
+import {createStyles, Theme, useTheme} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
 
 export type HomePageProps = HomePageDataProps & HomePageStyleProps & HomePageEventProps;
 
@@ -11,20 +13,26 @@ export interface HomePageStyleProps {
 }
 
 export interface HomePageEventProps {
-  handleLoaded(): void;
+
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+
+  }),
+);
+
 const HomePage: React.FC<HomePageProps> = (props) => {
+  const theme: Theme = useTheme();
+  const classes = useStyles();
+
   const {
-    handleLoaded,
+
   } = props;
-  useEffect(() => {
-    handleLoaded();
-  }, []);
 
   return (
     <div/>
-  )
+  );
 };
 
 export default HomePage;
