874d3c9543a0960c592864ccf211cb9d5b0e1ade
Kevin <yupolin921021@gmail.com>
2021-04-24 17:10:27 -0700

    Added update utils.

---
+++ server/src/service/Update/UpdateUtils.ts
@@ -0,0 +1,48 @@
+import {CasesUtils} from "../Cases/CasesUtils";
+import {TreeUtils} from "../Tree/TreeUtils";
+
+const moment = require("moment");
+
+export namespace UpdateUtils {
+  export let lastUpdate: string = moment().format("M/D/YY");
+
+  export const setupUpdate = () => {
+    setInterval(() => {
+      const lastUpdateMomentDate = moment(lastUpdate, "M/D/YY").startOf("day");
+      const currentMomentDate = moment();
+      const difference: number = currentMomentDate.diff(lastUpdateMomentDate);
+      const shouldUpdate: boolean = difference > 82800000;
+      if (shouldUpdate) {
+        updateData();
+      }
+    }, 3600000);
+  };
+
+  export const updateData = async () => {
+    try {
+      console.log(`Starting update. Time: ${moment().format("MMMM Do YYYY, h:mm:ss a")}.`);
+      //Clearing Data
+      CasesUtils.data = {};
+      CasesUtils.dailyInfoData = {};
+      CasesUtils.weeklyInfoData = {};
+      CasesUtils.monthlyInfoData = {};
+      CasesUtils.yearlyInfoData = {};
+      TreeUtils.data = {
+        hierarchicalName: "World",
+        countryCode: "World",
+        children: [],
+      };
+
+      await CasesUtils.fetchCasesData();
+      TreeUtils.getTreeData();
+
+      lastUpdate = moment().format("M/D/YY");
+      console.log(`Update Successful. Time: ${moment().format("MMMM Do YYYY, h:mm:ss a")}.`);
+      return true;
+    } catch (e) {
+      console.log(`Update Failed! Last Update: ${lastUpdate}. Current Time: ${moment().format("MMMM Do YYYY, h:mm:ss a")}.`);
+      console.log(e);
+      return false;
+    }
+  }
+}
