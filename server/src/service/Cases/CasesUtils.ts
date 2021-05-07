6f40d162de0a2d84a881558ada23d89f654880ec
Kevin <yupolin921021@gmail.com>
2021-05-07 09:50:23 -0700

    Revert "test"
    This reverts commit 56a7323c6d71fc33ef091885b4c83981f055b352.

--- server/src/service/Cases/CasesUtils.ts
+++ server/src/service/Cases/CasesUtils.ts
@@ -1343,7 +1343,7 @@ export namespace CasesUtils {
       sortData();
       fixUSData();
       generateWorldData();
-      generateCasesInformationData();
+      // generateCasesInformationData();
 
       return true;
     } catch (e) {
